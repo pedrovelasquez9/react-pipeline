353a2d710ea292f9d317f076b2473e4a57a9ad88
Pedro Plasencia <programacion.en.esp@gmail.com>
2022-09-11 15:31:08 +0200

    bugfix: workflows directory name typeo

--- src/About.tsx
+++ src/About.tsx
@@ -5,6 +5,7 @@ function About() {
     <div className="App">
       <h1>About page</h1>
       <p className="read-the-docs">Welcome to the About page!</p>
+      <p>Programación en español</p>
     </div>
   );
 }
